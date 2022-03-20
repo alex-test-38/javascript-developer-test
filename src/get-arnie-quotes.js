a79d8409a12de0e680ba6edfa4fb327086af71aa
ALEX CHEN <alex.chen@sdce.com.au>
2022-03-20 20:21:45 +1100

    getArnieQuotes Implementation

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,9 +1,21 @@
+const { createTestScheduler } = require('jest');
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
   // TODO: Implement this function.
   // return results;
-};
+  const httpRequests = urls.map(url => httpGet(url))
+  const responses = await Promise.all(httpRequests)
+  const results = responses.map(res => {
+    const value = JSON.parse(res.body)
+    if (res.status === 200) {
+      return { 'Arnie Quote': value.message }
+    } else {
+      return { 'FAILURE': value.message }
+    }
+  })
+  return results
+}
 
 module.exports = {
   getArnieQuotes,
