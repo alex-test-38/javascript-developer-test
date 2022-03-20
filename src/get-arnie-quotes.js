4e4fc4cfe72612728b7f8f79ddbb12e747b5fdcb
ALEX CHEN <alex.chen@sdce.com.au>
2022-03-20 23:04:04 +1100

    version 2

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -4,16 +4,31 @@ const { httpGet } = require('./mock-http-interface');
 const getArnieQuotes = async (urls) => {
   // TODO: Implement this function.
   // return results;
-  const httpRequests = urls.map(url => httpGet(url))
-  const responses = await Promise.all(httpRequests)
-  const results = responses.map(res => {
-    const value = JSON.parse(res.body)
-    if (res.status === 200) {
-      return { 'Arnie Quote': value.message }
-    } else {
-      return { 'FAILURE': value.message }
-    }
-  })
+
+  // version 1 with two loops
+  // const httpRequests = urls.map(url => httpGet(url))
+  // const responses = await Promise.all(httpRequests)
+  // const results = responses.map(res => {
+  //   const value = JSON.parse(res.body)
+  //   if (res.status === 200) {
+  //     return { 'Arnie Quote': value.message }
+  //   } else {
+  //     return { 'FAILURE': value.message }
+  //   }
+  // })
+
+  //version 2 more compact with only one loop
+  const results = Promise.all(
+    urls.map(async (url) => {
+      const response = await httpGet(url)
+      const value = JSON.parse(response.body)
+      if (response.status === 200) {
+        return { 'Arnie Quote': value.message }
+      } else {
+        return { 'FAILURE': value.message }
+      }
+    })
+  )
   return results
 }
 
